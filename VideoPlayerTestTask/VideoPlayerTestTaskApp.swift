188d7044b7390fa45775f42385ff9e02d6174cb0
Nikita <nikita2907200303@gmail.com>
2024-03-24 13:25:37 +0300

    Initial Commit

---
+++ VideoPlayerTestTask/VideoPlayerTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerTestTaskApp.swift
+//  VideoPlayerTestTask
+//
+//  Created by Никита Иванов on 24.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
